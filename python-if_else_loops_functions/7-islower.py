9394a11181184cf2e2b99ffe9cc5192ea37b58a2
Owen Ganza <o.ganza@alu.com>
2024-10-24 04:44:51 -0700

    commit all

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print("{}{}".format(i, j))
-            break
-        print("{}{}".format(i, j), end=", ")
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
