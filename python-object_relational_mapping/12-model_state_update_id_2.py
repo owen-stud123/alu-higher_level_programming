bf0ced17db374994ffe395a082a6e82f700de402
Owen Ganza <o.ganza@alu.com>
2024-11-28 11:23:57 -0800

    commit all

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-"""script that prints the State object with the name passed as argument
+"""script that changes the name of a State object
 from the database hbtn_0e_6_usa
 """
 
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
     Base.metadata.create_all(engine)
-    session = Session(engine)
 
-    state_name = sys.argv[4]
+    state = session.query(State).filter(State.id == 2).first()
 
-    state = session.query(State).filter(State.name == state_name).first()
-    if state is None:
-        print("Not found")
-    else:
-        print(f'{state.id}')
+    state.name = "New Mexico"
 
+    session.commit()
     session.close()
